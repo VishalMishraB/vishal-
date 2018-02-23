3c031c3241700fd71cfb5b77d6d3eb4f959d087b
VishalMishraB <35587825+VishalMishraB@users.noreply.github.com>
2018-02-23 10:28:11 +0530

    Add files via upload

---
+++ programe_function.py
@@ -0,0 +1,17 @@
+#function
+def add(a,b):
+    print(a+b)
+
+def sub(x,y):
+    print(x-y)
+
+def multiply(c,d):
+    print(c*d)
+
+def divide(e,f):
+    print(e/f)
+
+add(100,300)
+sub(10,400)
+multiply(15,15)
+divide(1000,100)
